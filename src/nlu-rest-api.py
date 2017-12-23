06671b764f98fd879ad2395e9188dda75d9b7205
Geoff Collis <gcollis@ymail.com>
2017-12-23 21:12:54 +0100

    cleanup and add new CLI options

--- src/nlu-rest-api.py
+++ src/nlu-rest-api.py
@@ -5,11 +5,11 @@ import sys
 import os
 
 
-def call_watson_nlu(cfg):
+def simple_nlu_analyse(cfg, url='www.ibm.com', filename=None):
     import json
     import requests
     from ConfigParser import SafeConfigParser
-    
+
     config = SafeConfigParser()
     config.read(cfg)
 
@@ -19,24 +19,47 @@ def call_watson_nlu(cfg):
     api = "https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze"
     wuser = config.get('watson', 'username')
     wpass = config.get('watson', 'password')
-    payload = {'version': '2017-02-27', 'url': 'www.ibm.com', 'features': 'sentiment,keywords'}
+    payload = {'version': '2017-02-27', 'url': url, 'features': 'sentiment,keywords'}
     r = requests.get(api, params=payload, auth=(wuser, wpass))
     if args.verbose >= 1:
-        print(r.url)
+        if filename is None:
+            print('URL: ' + url)
+        else:
+            print('File: ' + filename)
             
-    print(json.dumps(r.text, indent=2))
+        print('Request: ' + r.url)
+
+    #print(json.dumps(r.text, sort_keys=True, indent=2, separators=(',', ': ')))
+    print(r.text)
+
+
+
+def nlu_json_analyse(cfg, url='www.ibm.com', filename=None):
+    import json
+    import requests
+    from ConfigParser import SafeConfigParser
+
+    config = SafeConfigParser()
+    config.read(cfg)
+
+    pass
 
 
 if __name__ == "__main__":
     watson_cfg_file = os.path.join(os.getcwd(), '.watson.cfg')
     parser = argparse.ArgumentParser(description='Simple NLU Category example')
-    parser.add_argument('-v', '--verbose', action='count', default=0)
     parser.add_argument('-c', '--cfg', default=watson_cfg_file, help='Watson credentials')
+    parser.add_argument('-f', '--filename', default=None, help='text file to analyze')
+    parser.add_argument('-u', '--url', default='www.ibm.com', help="URL to analyze")
+    parser.add_argument('-j', '--json', default=None, help="JSON parameters")
+    parser.add_argument('-v', '--verbose', action='count', default=0)
     args = parser.parse_args()
-    if args.verbose >= 1 :
+    if args.verbose >= 1:
         print "watson-cfg: " + args.cfg
-        
-    call_watson_nlu(cfg=args.cfg)
-    
+
+    if args.json:
+        nlu_json_analyse(cfg=args.cfg, url=args.url, filename=args.filename)
+    else:
+        simple_nlu_analyse(cfg=args.cfg, url=args.url, filename=args.filename)
+
     sys.exit(0)
-    
