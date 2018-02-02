5de80efb98684cbb8be92e0b1b3418f2a82f857d
Geoff Collis <gcollis@ymail.com>
2018-02-02 19:19:56 +0100

    refactor hiding global variables

--- src/WDSCredentials.py
+++ src/WDSCredentials.py
@@ -11,23 +11,26 @@ class WDSCredentials:
     IBM Watson Discovery Login Credentials
     '''
 
+    #===========================================================================
+    # __init__
+    #===========================================================================
     def __init__(self, filename):
-        self.username = None
-        self.password = None
-        self.version = '2017-11-07'
-        self.url = 'https://gateway.watsonplatform.net/discovery/api'
+        self.__username = None
+        self.__password = None
+        self.__version = '2017-11-07'
+        self.__url = 'https://gateway.watsonplatform.net/discovery/api'
 
         with open(filename) as f:
             try:
                 data = json.loads(f.read())
                 if 'username' in data:
-                    self.username = data['username']
+                    self.__username = data['username']
                 if 'password' in data:
-                    self.password = data['password']
+                    self.__password = data['password']
                 if 'url' in data:
-                    self.url = data['url']
+                    self.__url = data['url']
                 if 'version' in data:
-                    self.version = data['version']
+                    self.__version = data['version']
 
             except ValueError:
                 config = SafeConfigParser()
@@ -35,28 +38,76 @@ class WDSCredentials:
                 data = config.items('discovery')
                 for d in data:
                     if d[0] == 'username':
-                        self.username = d[1]
+                        self.__username = d[1]
                     if d[0] == 'password':
-                        self.password = d[1]
+                        self.__password = d[1]
                     if d[0] == 'url':
-                        self.url = d[1]
+                        self.__url = d[1]
                     if d[0] == 'version':
-                        self.version = d[1]
+                        self.__version = d[1]
+
+    #===========================================================================
+    # credentials
+    #===========================================================================
+    def credentials(self):
+        '''
+        Return Watson Discovery Service Credentials
+        '''
+        return {'username': self.__username, 'password': self.__password, 'url': self.__url, 'version': self.__version}
+
+    #===========================================================================
+    # username
+    #===========================================================================
+    def username(self):
+        '''
+        Return Watson Discovery Service username
+        '''
+        return self.__username
+
+    #===========================================================================
+    # password
+    #===========================================================================
+    def password(self):
+        '''
+        Return Watson Discovery Service Password
+        '''
+        return self.__password
+
+    #===========================================================================
+    # url
+    #===========================================================================
+    def url(self):
+        '''
+        Return Watson Discovery Service API URL
+        '''
+        return self.__url
+
+    #===========================================================================
+    # version
+    #===========================================================================
+    def version(self):
+        '''
+        Return Watson Discovery Service Version
+        '''
+        return self.__version
+
 
 if __name__ == "__main__":
 
     wds = WDSCredentials('.gc.watson.json')
     print "JSON:"
-    print "username: {0}".format(wds.username)
-    print "password: {0}".format(wds.password)
-    print "url: {0}".format(wds.url)
-    print "version: {0}".format(wds.version)
+    print wds.credentials()
+    print "username: {0}".format(wds.username())
+    print "password: {0}".format(wds.password())
+    print "url: {0}".format(wds.url())
+    print "version: {0}".format(wds.version())
+    print
 
     wds = WDSCredentials('.gc.watson.cfg')
-    print "Configuration:"
-    print "username: {0}".format(wds.username)
-    print "password: {0}".format(wds.password)
-    print "url: {0}".format(wds.url)
-    print "version: {0}".format(wds.version)
+    print wds.credentials()
+    print "username: {0}".format(wds.username())
+    print "password: {0}".format(wds.password())
+    print "url: {0}".format(wds.url())
+    print "version: {0}".format(wds.version())
 
     sys.exit(0)
