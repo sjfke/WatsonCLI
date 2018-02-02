333b4033303397e17126834c9cb18eaeaf20d4e5
Geoff Collis <gcollis@ymail.com>
2018-02-02 19:40:19 +0100

    add simple test script

--- src/WDSCredentials.py
+++ src/WDSCredentials.py
@@ -90,24 +90,3 @@ class WDSCredentials:
         Return Watson Discovery Service Version
         '''
         return self.__version
-
-
-if __name__ == "__main__":
-
-    wds = WDSCredentials('.gc.watson.json')
-    print "JSON:"
-    print wds.credentials()
-    print "username: {0}".format(wds.username())
-    print "password: {0}".format(wds.password())
-    print "url: {0}".format(wds.url())
-    print "version: {0}".format(wds.version())
-    print
-
-    wds = WDSCredentials('.gc.watson.cfg')
-    print wds.credentials()
-    print "username: {0}".format(wds.username())
-    print "password: {0}".format(wds.password())
-    print "url: {0}".format(wds.url())
-    print "version: {0}".format(wds.version())
-
-    sys.exit(0)
